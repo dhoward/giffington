681c67ced5422b56fd9b421fd5ae4d5d3b896e80
Daniel Howard <dan.howard@originate.com>
2013-01-22 22:58:10 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Giffington::Application.config.secret_token = 'a6427257c859225920e68203b9730dcb20f2cfe8768e14dad6c664c0da243c5c53b17ddeabb9d5cb38ffb911e7af6d660595e234c334898934647e00313cb6ac'
